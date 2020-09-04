55821a88389ee1f606bd8ca8eced5b3b14c9a28f
Phil Starck <pstarc1@gmail.com>
2020-09-04 09:48:54 -0400

    Important commit. The weather app works and adjusts color depending on the temperature. This is a good starting point for the template pof a React project to change background gradient or image on data from fetch rest api consumed.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import DisplayMain from './components/presentational/DisplayMain'
+import DisplayMain from './components/presentational/DisplayMain';
 
-console.log(process.env.REACT_APP_GOOGLE_API_KEY)
+console.log(process.env.REACT_APP_OWM_API_KEY)
 
 function App() {
   return (
--- src/components/presentational/DisplayMain.js
+++ src/components/presentational/DisplayMain.js
@@ -3,16 +3,15 @@ import SubscribersView from './SubscribersView';
 import EditorsView from './EditorsView';
 import AdminsView from './AdminsView';
 
+import Weather from '../weather/Weather';
+
 export default function DisplayMain() {
   return (
     <div>
-
+      <Weather />
       <SubscribersView />
-
       <EditorsView />
-
       <AdminsView />
-      
     </div>
   )
 }
