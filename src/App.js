2d5be06c65e60b60e3bb137a25cdc20800050acd
Phil Starck <pstarc1@gmail.com>
2020-09-04 08:16:05 -0400

    gitignore update.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import DisplayMain from './components/presentational/DisplayMain'
 
+console.log(process.env.REACT_APP_GOOGLE_API_KEY)
+
 function App() {
   return (
     <div className="App">
